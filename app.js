c2fa4398ca306f3c791fc9d52eb000943bf927af
hlinforest <hllin@sfu.ca>
2015-11-12 15:39:00 -0800

    file permission/type, absolute url

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
 var csv = require('csv-write-stream');
+var path = require('path');
 
 request('http://substack.net/images/', function (error, response, body) {
   // if (!error && response.statusCode == 200) {
@@ -35,9 +36,13 @@ request('http://substack.net/images/', function (error, response, body) {
   }
   else{
     $ = cheerio.load(body);
-    $('tr').each(function(i, elem){
-      var permission = $(this).find('td').first().text();
-      console.log(permission);
+    $('tr').each(function(index, element){
+      var file_permission = $(this).find('td').first().text();
+      var absolute_url = $(this).find('td:nth-child(3)').find('a').attr('href');
+      var file_type = path.extname($(this).find('td:nth-child(3)').text());
+      console.log(file_permission);
+      console.log(absolute_url);
+      console.log(file_type);
     });
   }
 })
